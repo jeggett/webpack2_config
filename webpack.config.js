81154789383f7b96e0d389c723ee798a9cf29cdf
Alexander Zaytsev <jeggett@mail.ru>
2017-01-04 03:06:20 +0300

    feat: add CaseSensitivePathPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
@@ -17,6 +18,9 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Webpack initial',
+      // add html-webpack-favicon and custom template with yandex map api
+      // it's also possible to add webpack-dashboard later
     }),
+    new CaseSensitivePathsPlugin(),
   ],
 };
