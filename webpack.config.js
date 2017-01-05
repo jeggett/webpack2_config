afbc1b697da58d6e2ee24ede3e88f46b97f63d80
Alexander Zaytsev <jeggett@mail.ru>
2017-01-05 18:08:25 +0300

    feat: Add webpack-merge to branch or dev/prod mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,34 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const merge = require('webpack-merge');
 
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack initial',
-      // add html-webpack-favicon and custom template with yandex map api
-      // it's also possible to add webpack-dashboard later
-    }),
-    new CaseSensitivePathsPlugin(),
-  ],
+const common = merge (
+  {
+    entry: {
+      app: PATHS.app,
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].js',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Webpack initial',
+        // add html-webpack-favicon and custom template with yandex map api
+        // it's also possible to add webpack-dashboard later
+      }),
+      new CaseSensitivePathsPlugin(),
+    ],
+  }
+);
+
+module.exports = function (env) {
+  console.log(env)
+  return merge(common);
 };
